5bb25ba9e196baecd0a2dad6c4edf6233343b5d9
zacharynarcotta <zachary.narcotta@maine.edu>
2025-02-21 16:25:02 -0500

    Create Book constructor and toString method

--- Book.java
+++ Book.java
@@ -1,3 +1,14 @@
 public class Book {
-    
+    public String title;
+    public String genre;
+
+    public Book(String title, String genre) {
+        this.title = title;
+        this.genre = genre;
+    }
+
+    @Override
+    public String toString() {
+        return title + "\n";
+    }
 }
